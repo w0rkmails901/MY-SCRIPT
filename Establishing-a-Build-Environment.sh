da78bc9b9f3aa4d9d9f5a1bdbd15a83d1e3fed0d
w0rkmails901 <w0rkmails901@gmail.com>
2016-06-27 21:38:14 +0200

    Update Establishing-a-Build-Environment.sh

--- Establishing-a-Build-Environment.sh
+++ Establishing-a-Build-Environment.sh
@@ -1,17 +1,6 @@
 # Created By Hamada Jimmy "GMCadimo" <gm.cadiom@gmail.com>
 # CopyRights To Me And My WebSite www.docroid.tk
 clear
-echo Installing Dependencies!
-sudo apt-get -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev \
-squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre openjdk-7-jdk pngcrush \
-schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
-gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool \
-automake g++ gawk subversion expat libexpat1-dev python-all-dev binutils-static bc libcloog-isl-dev \
-libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* \
-liblzma* w3m phablet-tools android-tools-adb ccache maven ncftp
-sudo apt-get -y install lib32z-dev libx11-dev x11proto-core-dev libc6-dev-i386
-sudo apt-get -y install meven2 libgl1-mesa-dev unzip
-echo Dependencies have been installed 
 echo install java!
 sudo apt-get install openjdk-7-jre
 sudo apt-get install openjdk-7-jdk
