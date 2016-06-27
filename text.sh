c7679a0e88799a685f41c69d2c78051ba750166f
Hamada Jimmy <gm.cadiom@gmail.com>
2016-06-27 06:10:47 +0300

    Update text.sh

--- text.sh
+++ text.sh
@@ -4,11 +4,11 @@ echo Installing Dependencies!
 sudo apt-get update
 sudo apt-get -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev \
 squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre openjdk-7-jdk pngcrush \
-schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
+schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev-i386 schedtool g++-multilib lib32z1-dev lib32z-dev libx11-dev lib32ncurses5-dev x11proto-core-dev \
 gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool \
 automake g++ gawk subversion expat libexpat1-dev python-all-dev binutils-static bc libcloog-isl-dev \
 libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* \
-liblzma* w3m phablet-tools android-tools-adb ccache maven ncftp
+liblzma* w3m phablet-tools android-tools-adb ccache maven meven2 ncftp libgl1-mesa-dev unzip
 echo Dependencies have been installed
 echo repo has been Downloaded!
 if [ ! "$(which adb)" == "" ];
